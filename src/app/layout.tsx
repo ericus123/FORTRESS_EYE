c655419cf295edf586f8fb8d2d59b8d388f482ed
AMANI Eric <amaniericus@gmail.com>
2023-12-20 01:03:29 +0200

    ft(setup): add graphql setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import { withUrqlClient } from "next-urql";
 import { Inter } from "next/font/google";
 import ThemeRegistry from "../components/theme/ThemeRegistry";
 import { colors } from "../constants/colors";
+import { clientOptions } from "../graphql/useClient";
 import { Providers } from "../redux/provider";
 import "../styles/fonts.scss";
 
@@ -9,14 +11,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "FortressEye",
-  description: "Home automation system (IOT)"
+  description: "Home Smart System"
 };
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body
@@ -30,4 +28,6 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
+};
+
+export default withUrqlClient(() => clientOptions, { ssr: true })(RootLayout);
---
+++ src/components/dashboard/alarms/Alarm.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const Alarm = () => {
+  return <Box></Box>;
+};
+
+export default Alarm;
