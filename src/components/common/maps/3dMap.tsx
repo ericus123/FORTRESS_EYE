a94c9880ed4472bde63d7512c6622aab10b81e98
AMANI Eric <amaniericus@gmail.com>
2024-02-03 10:26:10 +0200

    ft(map): implement base maps ui

---
+++ src/components/common/maps/3dMap.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const ThreeDMap = () => {
+  return <Box></Box>;
+};
+
+export default ThreeDMap;
---
+++ src/components/dashboard/map/index.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const AppMap = () => {
+  return <Box></Box>;
+};
+
+export default AppMap;
